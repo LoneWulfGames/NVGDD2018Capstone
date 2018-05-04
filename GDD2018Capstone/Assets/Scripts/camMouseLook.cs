dbf9a47ca8a20f05d1637e630bf0afd759ed33cf
Raptorwolffe <raptorprowl@gmail.com>
2018-05-04 09:43:17 -0400

    Mouse look
    Revised the mouse look script...it appears to be working as desired now

--- GDD2018Capstone/Assets/Scripts/camMouseLook.cs
+++ GDD2018Capstone/Assets/Scripts/camMouseLook.cs
@@ -7,19 +7,29 @@ using UnityEngine;
 
 public class camMouseLook : MonoBehaviour {
 
-    public float speedH = 2.0f;
-    public float speedV = 2.0f;
+    Vector2 mouseLook;
+    Vector2 smoothV;
+    public float sensitivity = 5.0f;
+    public float smoothing = 2.0f;
 
-    private float yaw = 0.0f;
-    private float pitch = 0.0f;
+    GameObject character;
 
-    void Update() {
-        yaw += speedH * Input.GetAxis("Mouse X");
-        pitch -= speedV * Input.GetAxis("Mouse Y");
-
-        transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
-    }
-
+	// Use this for initialization
+	void Start () {
+        character = this.transform.parent.gameObject;
 	}
+	
+	// Update is called once per frame
+	void Update () {
+        var md = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
 
+        md = Vector2.Scale(md, new Vector2(sensitivity * smoothing, sensitivity * smoothing));
+        smoothV.x = Mathf.Lerp(smoothV.x, md.x, 1f / smoothing);
+        smoothV.y = Mathf.Lerp(smoothV.y, md.y, 1f/ smoothing);
+        mouseLook += smoothV;
+        mouseLook.y = Mathf.Clamp(mouseLook.y, -90f, 90f);
 
+        transform.localRotation = Quaternion.AngleAxis(-mouseLook.y, Vector3.right);
+        character.transform.localRotation = Quaternion.AngleAxis(mouseLook.x, Vector3.up);
+	}
+}
