8c8f2b66cbcdd11e260ae06a94dbf0c56c198b8f
Tony Matos <matosman1209@gmail.com>
2018-05-09 08:23:43 -0400

    Working on Multiplayer characters over network

---
+++ GDD2018Capstone/Assets/Scripts/CameraControlOther.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+public class CameraControlOther : NetworkBehaviour
+{
+    public Vector2 mouseLook;
+    private Vector2 smoothV;
+    public float sensitivity = 5.0f;
+    public float smoothing = 2.0f;
+    //person we are seeing through
+    public GameObject mob;
+    public Vector3 offSetFromMob;
+    public float tiltMin, tiltMax;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Cursor.lockState == CursorLockMode.None)
+        { return; }
+        //get mouse direction
+        var md = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+        //smooth mouse direction
+        smoothV.x = Mathf.Lerp(smoothV.x, md.x, 1f / smoothing);
+        smoothV.y = Mathf.Lerp(smoothV.y, md.y, 1f / smoothing);
+        mouseLook += smoothV;
+        mouseLook = new Vector2(mouseLook.x, Mathf.Clamp(mouseLook.y, -30, 30));
+
+        transform.localRotation = Quaternion.AngleAxis(-mouseLook.y, Vector3.right);
+        if (mob != null)
+            mob.transform.localRotation = Quaternion.AngleAxis(mouseLook.x, mob.transform.up);
+    }
+    public void ResetCam()
+    {
+        //print("setting camera up!");    
+        transform.SetParent(mob.transform);
+        //transform.position=(mob.transform.position+ Vector3.up);
+        transform.position = mob.transform.position + offSetFromMob;
+    }
+}
--- GDD2018Capstone/Assets/Scripts/PlayerController.cs
+++ GDD2018Capstone/Assets/Scripts/PlayerController.cs
@@ -3,60 +3,63 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 
+//created by Ariel
+//deals with player movement
+//directly associated with camMouseLook script
+
 public class PlayerController : NetworkBehaviour
 {
-    public float speed = 0.2f;
-    public float camY;
-    public Camera cam;
 
-    void Update ()
+    public float speed = 10.0f;
+
+    public CharacterController player;
+
+    public Camera cam;
+    public CameraControlOther camControl;
+
+    // Use this for initialization
+
+    void Start()
     {
-        if(!isLocalPlayer)
+        Cursor.lockState = CursorLockMode.Locked; //game cursor cannot be seen in game
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isLocalPlayer)
         {
             return;
         }
 
-        cam = FindObjectOfType<Camera>();
+        /*float translation = Input.GetAxis("Vertical") * speed;
+        float straffe = Input.GetAxis("Horizontal") * speed;
+        translation *= Time.deltaTime;
+        straffe *= Time.deltaTime;
 
-        camY = cam.transform.rotation.y;
+        transform.Translate(straffe, 0, translation);*/
 
-        /*float x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+        float x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
         float z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
 
         transform.Rotate(0, x, 0);
-        transform.Translate(0, 0, z);*/
+        transform.Translate(0, 0, z);
 
-        if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
-        {
-            transform.Translate(Vector3.left * speed);
-        }
-
-        else if(Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
-        {
-            transform.Translate(Vector3.right * speed);
-        }
-
-        else if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
-        {
-            transform.Translate(Vector3.forward * speed);
-        }
-
-        else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
-        {
-            transform.Translate(Vector3.back * speed);
-        }
-
-        CameraFollow();
-
-    }
-
-    public void CameraFollow()
-    {
-        
+        if (Input.GetKeyDown("escape"))
+            Cursor.lockState = CursorLockMode.None;
     }
 
     public override void OnStartLocalPlayer()
     {
         GetComponent<MeshRenderer>().material.color = Color.yellow;
+
+        //prevent cursor usage
+        Cursor.lockState = CursorLockMode.Locked;
+        //setup character controller
+        player = GetComponent<CharacterController>();
+        cam = Camera.main;
+        camControl = cam.GetComponent<CameraControlOther>();
+        camControl.mob = this.gameObject;
+        camControl.ResetCam();
     }
 }
