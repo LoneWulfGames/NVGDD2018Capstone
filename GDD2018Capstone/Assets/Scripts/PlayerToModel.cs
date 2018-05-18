b6fd37eb6589374c7dc2b9feda928f3446f7ff70
Tony Matos <matosman1209@gmail.com>
2018-05-18 09:41:12 -0400

    Added spriting

--- GDD2018Capstone/Assets/Scripts/PlayerToModel.cs
+++ GDD2018Capstone/Assets/Scripts/PlayerToModel.cs
@@ -4,16 +4,25 @@ using UnityEngine;
 
 public class PlayerToModel : MonoBehaviour {
 
+    GameObject selectedModel;
+    public GameObject model;
+
 	// Use this for initialization
-	void Start () {
-		
-	}
-    private void OnMouseDown()
+	void Start ()
     {
-        
-    }
-    // Update is called once per frame
-    void Update () {
-		
+        ShowPlayer();
 	}
+
+    public void SwapModel(GameObject newModel)
+    {
+        model = newModel;
+        Destroy(selectedModel);
+        ShowPlayer();
+    }
+
+    void ShowPlayer()
+    {
+        selectedModel = Instantiate(model, transform);
+    }
+
 }
