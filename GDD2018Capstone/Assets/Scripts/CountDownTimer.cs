4f11da0165c63be5e9f27d13a41ad8e816626b7e
Tony Matos <matosman1209@gmail.com>
2018-06-04 09:42:33 -0400

    Created the Countdown timer

---
+++ GDD2018Capstone/Assets/Scripts/CountDownTimer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountDownTimer : MonoBehaviour
+{
+    public float timeLeft = 180f;
+
+    public Text timerText;
+
+    public void Update()
+    {
+        timeLeft -= Time.deltaTime;
+
+        ChangeTimerText();
+
+        if(timeLeft <= 0f)
+        {
+            GameOver();
+        }
+    }
+
+    public void GameOver()
+    {
+        Debug.Log("GameOver!");
+    }
+
+    public void ChangeTimerText()
+    {
+        timerText.text = ("Time Left: " + timeLeft);
+        
+        if(timeLeft <= 30)
+        {
+            timerText.color = Color.red;
+        }
+    }
+}
